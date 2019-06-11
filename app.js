ad6834c37498e26bf514d11d4bb9939ebde99fb2
Aryan Likhite <aryanlikhite@gmail.com>
2019-06-11 20:29:42 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const auth = require("./auth.json");
+const Gamedig = require('gamedig');
+const client = new Discord.Client();
+
+const PREFIX = "."
+
+client.on('ready', () => {
+    console.log('Bot is ready!');
+});
+
+client.on('message', async message => {
+    if(message.content === `${PREFIX}online`) {
+        try{
+            let state =  await Gamedig.query({
+                type: 'mtasa',
+                host: '94.23.153.11'
+            })
+            let onlineUSSF = [];
+            state.players.forEach(player => {
+                if(player.name.match(/(USSF)/g)) {
+                    onlineUSSF.push(player.name);
+                }
+            })
+            let string = `There are **${onlineUSSF.length}** USSF Operators online. \n\`\`\``
+            onlineUSSF.forEach(player => {
+                string = string + player + "\n"
+            })
+            string = string + "\`\`\`"
+            message.channel.send(string);
+        } catch(err) {
+            console.log("Beep Boop! Something went wrong. Please blame Rex!");
+        }
+    }
+});
+
+const token = process.env.token || auth.token
+
+client.login(token);
